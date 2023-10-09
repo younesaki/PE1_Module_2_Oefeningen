d01f9ce8942597331eb0967893802f3c4aea17a2
Younes Aki <younesakichouh@hotmail.com>
2023-10-09 19:42:34 +0200

    Easy challenge 3 : done

---
+++ Easy_challenges/e3_dagenInDeMaand.mjs
@@ -0,0 +1,30 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+import { machine } from 'node:os';
+const userInput = readline.createInterface({input, output});
+
+let maand = await userInput.question('Geef een maand in:');
+
+switch(maand){
+    case 'januari':
+    case 'maart':
+    case 'mei':
+    case 'juli':
+    case 'augustus':
+    case 'oktober':
+    case 'december':
+        console.log('Deze maand heeft 31 dagen.');
+        break;
+    case 'Februari':
+        console.log('Deze maand heeft 28 dagen in een normaal jaar.');
+        break;
+    case 'april':
+    case 'juni':
+    case 'september':
+    case 'november':
+        console.log('Deze maand heeft 30 dagen.');
+        break;
+    default :
+        console.log('Je hebt geen geldige maand ingegeven.');
+        break;
+}
--- e3_dagenInDeMaand.mjs
+++
@@ -1,5 +0,0 @@
-import * as readline from 'node:readline/promises';
-import{stdin as input, stdout as output} from 'node:process';
-import { machine } from 'node:os';
-const userInput = readline.createInterface({input, output});
-
