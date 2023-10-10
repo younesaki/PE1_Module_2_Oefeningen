fd2367ca1009106b4ef3b10bc644c7633e174b71
Younes Aki <younesakichouh@hotmail.com>
2023-10-10 20:20:00 +0200

    m1 done

---
+++ Medium_challenges/m1_songtitels_ifElse.mjs
@@ -0,0 +1,30 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+let artiest1 = 'Dave' ;
+let artiest2 = 'Central Cee' ;
+let artiest3 = 'PNL' ;
+let artiest4 = 'Morad' ;
+
+console.log('Kies een artiest');
+console.log('1. ' + artiest1);
+console.log('2. ' + artiest2);
+console.log('3. ' + artiest3);  
+console.log('4. ' + artiest4); 
+let GebruikersKeuze = await userInput.question('Geef je keuze in:');
+
+if(GebruikersKeuze == '4'){
+    console.log('Je hebt gekozen voor Morad. Hij is de artiest achter de hit: Pelele');
+}else if(GebruikersKeuze == '3'){
+    console.log('Je hebt gekozen voor PNL. Zij zijn de artiesten achter de hit: DA');
+}else if(GebruikersKeuze == '2'){
+    console.log('Je hebt gekozen voor de artiest Central Cee. Hij is de artiest achter de hit: Doja');
+}else if(GebruikersKeuze == '1'){
+    console.log('Je hebt gekozen voor de artiest Dave. Hij is de artiest achter de hit: Verdansk');
+}else{
+    console.log('Je moet het getal ingeven dat voor de artiest staat.');
+}
+
+process.exit();
+
