45addabffcab772b7d72c6acf3395e4f5e517bbd
Younes Aki <younesakichouh@hotmail.com>
2023-10-15 22:15:25 +0200

    first challenge @ home done

---
+++ challenge_home/tweeVariabelen.mjs
@@ -0,0 +1,16 @@
+let var1 = 11;
+let var2 = "11";
+
+console.log(var1 + " en " + var2);
+
+if( var1 == var2 ){
+    console.log("Ze zijn gelijk");
+}else{
+    console.log("Ze zijn niet gelijk");
+}                                           // ze zijn gelijk na uitvoeren
+
+if( var1 === var2 ){
+    console.log("Ze zijn gelijk");
+}else{
+    console.log("Ze zijn niet gelijk");
+}                                           // ze zijn niet gelijk na uitvoeren
