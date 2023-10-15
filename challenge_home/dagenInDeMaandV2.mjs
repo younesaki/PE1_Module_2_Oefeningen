adf054d286b66b874a949944e1d89e9bd956ff10
Younes Aki <younesakichouh@hotmail.com>
2023-10-15 22:15:40 +0200

    Added a comment

---
+++ challenge_home/dagenInDeMaandV2.mjs
@@ -0,0 +1,5 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+// wat is het verschil van deze oefening met oefening e3?
